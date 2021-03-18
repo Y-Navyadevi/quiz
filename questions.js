c0783fa05818a70262935e63e708352701ff6a4c
Y-Navyadevi <navyadevi789@msitprogram.net>
2021-03-18 16:12:20 +0530

    Commit

---
+++ questions.js
@@ -0,0 +1,7 @@
+var questions = [
+  ["What is the color of Apple", "Yellow", "Blue", "Red","None", "3"],
+  ["What is the color of Mango?", "Black", "Yellow", " Red", "all","2"],
+  ["What is the color of Grapes?", "Green", "Red", "White", "all","1"],
+  ["What is the color of Brinjal?", "White", " Green", "Voilet", "Black","3"],
+  ["WHat is the color of Orange?", "Grey", "Orange", "Pink", "Lavender","2"],
+  ];
